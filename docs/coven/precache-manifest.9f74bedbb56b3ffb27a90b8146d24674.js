7d112fc3b5f53d067541640510215df373eef9a0
aio <tom.shegzy@gmail.com>
2019-08-31 20:58:33 +0100

    Coven App Added

---
+++ docs/coven/precache-manifest.9f74bedbb56b3ffb27a90b8146d24674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a6eaf0646f7a1a3f4657f25acd1a59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7969b51778492fe0e58c",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "070c9a450f053a7140a6",
+    "url": "/static/css/main.62b46332.chunk.css"
+  },
+  {
+    "revision": "7969b51778492fe0e58c",
+    "url": "/static/js/2.863ce2b5.chunk.js"
+  },
+  {
+    "revision": "070c9a450f053a7140a6",
+    "url": "/static/js/main.0d51c1dd.chunk.js"
+  },
+  {
+    "revision": "dfd223b09d7ab825a5dd",
+    "url": "/static/js/runtime~main.a473e9ec.js"
+  }
+]);
